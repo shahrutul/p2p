e1e2a85c57e88d15e9d0104319ee8e7ac4b88b9e
eugen.ruppert <eugen.ruppert@uni-duesseldorf.de>
2011-03-13 01:21:18 +0000

    So langsam (und endlich) wird's was. Netzwerkverbindung wird initiiert (sofern ein Bootstrapingpoint zur Verfuegung steht) und auf implementierte Nachrichten geantwortet.

--- core/signal_protocol.py
+++ core/signal_protocol.py
@@ -21,23 +21,25 @@ error(spec, reason)
 # ping: discover hosts on network
 ping(ping_id, ttl, hops)
 # args: string, number, number
-# returns: pong
+# returns: at least one pong
+
 
 # whoami: discover own IP address
 whoami()
-# returns: addr
+# returns: youare
 
 # query: search for data
 query(mydata, ttl, hops)
 # args: object, number, number
 # returns: query hit
 
-Datastructs:
 # pong: reply to ping with contact data
 pong(IP, port)
+# args: string, number
+# Pongs are only sent in response to an incoming ping
 
-# addr: IP address
-addr(IP, port)
+# youare: a reply to 'whoami'
+youare(IP, port)
 # args: string, number
 
 # query hit: reply to a query
@@ -77,7 +79,7 @@ class Signal():
     # implemented signals with name and argument types
     _types = {'error' : (basestring, basestring),
               'whoami' :(),
-              'addr': (basestring, int)
+              'youare': (basestring, int)
              }
     
     def __init__(self, type_, content = ()):        
@@ -86,8 +88,8 @@ class Signal():
             if not arg_type_check(arg_types, *content):
                 raise ProtocolError("Expected: %s(%s), not %s(%s)" %
                                     (type_, arg_types, type_, content))
-        except (ValueError, TypeError, KeyError):
-            raise ProtocolError(type_)
+        except (ValueError, TypeError, KeyError), reason:
+            raise ProtocolError("type = %s, content = %s" % (type_, content))
         
         self.type = type_
         self.content = content
@@ -100,14 +102,18 @@ class Signal():
         """ Deserializes JSON objects to Signals """
         logs.logger.debug("convert: %s" % data)
         try:
-            return Signal(*json.loads(data))
+            json_obj = json.loads(data)
+            return Signal(json_obj['type'], json_obj['content'])
         except (TypeError, ValueError), reason:
             raise ProtocolError(data, reason)
+        except KeyError:
+            err = """Expected: '{"%s":"x", "%s":["y","z"]}', not %s""" %\
+                  ('type', 'content', data)
+            raise ProtocolError(err)
 
     def serialize(self):
         """ Converts input to JSON objects
-        Returns: JSON representation, e.g 
-        """
+        Returns: JSON representation, e.g """
         logs.logger.debug("serialize: %s", self)
         try:
             return json.dumps({'type' :self.type,
@@ -116,54 +122,3 @@ class Signal():
             logs.logger.critical("serializer exception, reason %s" % reason)
             raise ProtocolError(reason)
         
-    
-        
-class Signal2(object):
-
-    TERMINATOR = '\0'
-    msg = namedtuple('msg', 'name args')
-    # Errors:
-    ProtocolError = "ProtocolError"
-    # Message names and argument types:
-    _error = msg('error', (basestring, basestring))
-    _ping = msg('ping',(basestring, basestring, basestring))
-    _whoami = msg('whoami',())
-    _addr = msg('addr', (basestring, int))
-    msg_pool = {'error' : _error,
-                'whoami' : _whoami}
-    
-    @staticmethod
-    def serialize(arg_msg):
-        """ Converts input to JSON objects, checks args types.
-        Accepts: Message.msg objects
-        Returns: JSON representation, e.g 
-        """
-        logs.logger.debug("serialize: msg(%s)", arg_msg)
-        try:
-            types = Signal.msg_pool[arg_msg.name].args
-            if arg_type_check(types, *arg_msg.args):
-                return json.dumps(arg_msg) + Signal.TERMINATOR
-            else:
-                raise ProtocolError("Expected: %s(%s), not %s(%s)" %
-                                    (arg_msg.name, types,
-                                     arg_msg.name, arg_msg.args))
-        except (KeyError, ValueError, TypeError), reason:
-            logs.logger.critical("serializer exception, reason %s" % reason)
-            raise ProtocolError(reason)
-        
-    @staticmethod
-    def deserialize(data):
-        """ Converts from JSON objects to message object, provides some
-        type checking."""
-        logs.logger.debug("deserialize: %s" % data)
-        try:
-            data_msg = Signal.msg(*json.loads(data))
-            types = Signal.msg_pool[data_msg.name].args
-            if not arg_type_check(types, *data_msg.args):
-                raise ProtocolError("Expected: %s(%s), not %s(%s)" %
-                                    (data_msg.name, types, data_msg.name,
-                                     tuple(map(type, data_msg.args))))
-            else:
-                return data_msg
-        except (ValueError, TypeError, KeyError), reason:
-            raise ProtocolError(data)
